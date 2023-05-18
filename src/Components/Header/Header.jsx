f20f5402e34b0b325bcc70e67ec8e4cf9784e347
nahid139 <nahidnhptake@gmail.com>
2023-05-17 21:53:36 -0700

    Tailwind added

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-6xl text-violet-300 bg-orange-600'>How are you</h1>
+        </div>
+    );
+};
+
+export default Header;
